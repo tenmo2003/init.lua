4c6806f0d4ab86296e63f09c5d0e392a59df48fb
Anthony <tenmo2003@gmail.com>
2025-06-09 16:47:19 +0700

    add git fugitive and change some telescope keybinds

--- lua/anhvn/lazy/telescope.lua
+++ lua/anhvn/lazy/telescope.lua
@@ -4,10 +4,11 @@ return {
 	config = function()
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
-		vim.keymap.set('n', '<leader>ps', function()
-			builtin.grep_string({ search = vim.fn.input("Grep > " )});
-		end)
+        vim.keymap.set('n', '<leader>pg', builtin.live_grep)
 		vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope git files' })
+        vim.keymap.set('n', '<leader>ps', function()
+            builtin.grep_string({ search = vim.fn.input("Grep > " )});
+        end)
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
@@ -16,6 +17,8 @@ return {
             local word = vim.fn.expand("<cWORD>")
             builtin.grep_string({ search = word })
         end)
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
+
+        vim.keymap.set('n', '<leader>pr', builtin.lsp_references, {})
 	end
 }
--- lua/anhvn/lazy/trouble.lua
+++ lua/anhvn/lazy/trouble.lua
@@ -4,7 +4,7 @@ return {
   cmd = "Trouble",
   keys = {
     {
-      "<leader>tt",
+      "<leader>xx",
       "<cmd>Trouble diagnostics toggle<cr>",
       desc = "Diagnostics (Trouble)",
     },
