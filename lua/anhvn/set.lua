73b2616f150b124e128f018de1e8812b6a87da7e
Anthony <tenmo2003@gmail.com>
2025-06-09 14:02:24 +0700

    done following theprimeagen video

--- lua/anhvn/set.lua
+++ lua/anhvn/set.lua
@@ -1,1 +1,34 @@
+vim.opt.nu = true
 vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+local home = vim.loop.os_uname().sysname == "Windows_NT"
+    and os.getenv("USERPROFILE") -- Windows
+    or os.getenv("HOME")         -- Unix-like
+
+vim.opt.undodir = home .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
