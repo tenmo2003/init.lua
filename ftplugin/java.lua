6b1e46d63dec1c8528fc96b6235eb7fd4a14eee3
Anthony <tenmo2003@gmail.com>
2025-06-10 17:10:08 +0700

    add java support with nvim-jdtls

---
+++ ftplugin/java.lua
@@ -0,0 +1,76 @@
+local home = vim.loop.os_uname().sysname == "Windows_NT"
+    and os.getenv("USERPROFILE") -- Windows
+    or os.getenv("HOME")         -- Unix-like
+
+local workspace_path = home .. '/.local/share/nvim/jdtls-workspace/'
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+local workspace_dir = workspace_path .. project_name
+
+local jdtls_path = vim.fs.joinpath(vim.fn.stdpath('data'), 'mason', 'packages', 'jdtls')
+
+local status, jdtls = pcall(require, 'jdtls')
+if not status then
+  return
+end
+local extendedClientCapabilities = jdtls.extendedClientCapabilities
+
+local config = {
+  cmd = {
+    'java',
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-Xmx1g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens',
+    'java.base/java.util=ALL-UNNAMED',
+    '--add-opens',
+    'java.base/java.lang=ALL-UNNAMED',
+    '-javaagent:' .. vim.fs.joinpath(jdtls_path, 'lombok.jar'),
+    '-jar',
+    vim.fn.glob(vim.fs.joinpath(jdtls_path, "plugins", 'org.eclipse.equinox.launcher_*.jar')),
+    '-configuration',
+    vim.fs.joinpath(jdtls_path, "config_win"),
+    '-data',
+    workspace_dir,
+  },
+  root_dir = require('jdtls.setup').find_root { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' },
+
+  settings = {
+    java = {
+      signatureHelp = { enabled = true },
+      extendedClientCapabilities = extendedClientCapabilities,
+      maven = {
+        downloadSources = true,
+      },
+      referencesCodeLens = {
+        enabled = true,
+      },
+      references = {
+        includeDecompiledSources = true,
+      },
+      inlayHints = {
+        parameterNames = {
+          enabled = 'all', -- literals, all, none
+        },
+      },
+      format = {
+        enabled = false,
+      },
+    },
+  },
+
+  init_options = {
+    bundles = {},
+  },
+}
+require('jdtls').start_or_attach(config)
+
+vim.keymap.set('n', '<leader>jo', "<Cmd>lua require'jdtls'.organize_imports()<CR>", { desc = 'Organize Imports' })
+vim.keymap.set('n', '<leader>jxv', "<Cmd>lua require('jdtls').extract_variable()<CR>", { desc = 'Extract Variable' })
+vim.keymap.set('v', '<leader>jxv', "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>", { desc = 'Extract Variable' })
+vim.keymap.set('n', '<leader>jxc', "<Cmd>lua require('jdtls').extract_constant()<CR>", { desc = 'Extract Constant' })
+vim.keymap.set('v', '<leader>jxc', "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>", { desc = 'Extract Constant' })
+vim.keymap.set('v', '<leader>jxm', "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", { desc = 'Extract Method' })
