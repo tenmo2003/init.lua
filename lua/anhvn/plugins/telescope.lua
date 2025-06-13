1678e133ba9a15220bdf4402ca6ba96b9a0d40ba
tenmo <tenmo2003@gmail.com>
2025-06-13 09:38:41 +0700

    correct show hidden setting of telescope and oil

--- lua/anhvn/plugins/telescope.lua
+++ lua/anhvn/plugins/telescope.lua
@@ -1,11 +1,18 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function()
-		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        require("telescope").setup {
+            pickers = {
+                find_files = {
+                    hidden = true,
+                }
+            }
+        }
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
         vim.keymap.set('n', '<leader>pg', builtin.live_grep)
-		vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope git files' })
+        vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope git files' })
         vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({ search = vim.fn.input("Grep > " )});
         end)
@@ -20,5 +27,5 @@ return {
         vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
 
         vim.keymap.set('n', '<leader>pr', builtin.lsp_references, {})
-	end
+    end
 }
