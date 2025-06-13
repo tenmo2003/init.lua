0e21b12a0011bad071ad980cdc83a0f85dfb7743
tenmo <tenmo2003@gmail.com>
2025-06-13 09:39:03 +0700

    only change undotree diffcommand on windows

--- lua/anhvn/plugins/undotree.lua
+++ lua/anhvn/plugins/undotree.lua
@@ -1,7 +1,9 @@
 return {
-	"mbbill/undotree",
-	config = function()
-		vim.g.undotree_DiffCommand = "FC"
-		vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
-	end
+    "mbbill/undotree",
+    config = function()
+        if vim.loop.os_uname().sysname == "Windows_NT" then
+            vim.g.undotree_DiffCommand = "FC"
+        end
+        vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+    end
 }
