ecc88bde46edf4c2eefc21f3603ebf75eb007500
tenmo <tenmo2003@gmail.com>
2025-06-20 13:36:09 +0700

    add formatter specification for javascript

--- lua/anhvn/plugins/conform.lua
+++ lua/anhvn/plugins/conform.lua
@@ -5,7 +5,11 @@ return {
         require("conform").setup({
             formatters_by_ft = {
                 lua = { "stylua" },
-                go = { "gofmt" }
+                go = { "gofmt" },
+                javascript = { "prettier" },
+                javascriptreact = { "prettier" },
+                typescript = { "prettier" },
+                typescriptreact = { "prettier" },
             }
         })
     end
