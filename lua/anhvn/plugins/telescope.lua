b8f579556a16a9358df40d6692769dd7f333fb55
tenmo <tenmo2003@gmail.com>
2025-06-23 17:49:13 +0700

    add keymap for telescope git commits

--- lua/anhvn/plugins/telescope.lua
+++ lua/anhvn/plugins/telescope.lua
@@ -15,17 +15,18 @@ return {
         vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope git files' })
         vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({ search = vim.fn.input("Grep > " )});
-        end)
+        end, { desc = "Search string" })
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
-        end)
+        end, { desc = "Search word under the cursor" })
         vim.keymap.set('n', '<leader>pWs', function()
             local word = vim.fn.expand("<cWORD>")
             builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
+        end, { desc = "Search WORD under the cursor" })
+        vim.keymap.set('n', '<leader>ph', builtin.help_tags, { desc = "View help tags" })
+        vim.keymap.set('n', '<leader>pc', builtin.git_commits, { desc = "View commits" })
 
-        vim.keymap.set('n', '<leader>pr', builtin.lsp_references, {})
+        vim.keymap.set('n', '<leader>pr', builtin.lsp_references, { desc = "View LSP references" })
     end
 }
