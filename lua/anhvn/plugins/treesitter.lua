45746ba579103fc04b02edf4a5dcfd915a1c2097
tenmo <tenmo2003@gmail.com>
2025-06-24 09:53:49 +0700

    add tsx language parser for treesitter

--- lua/anhvn/plugins/treesitter.lua
+++ lua/anhvn/plugins/treesitter.lua
@@ -17,6 +17,7 @@ return {
                     "markdown_inline",
                     "javascript",
                     "typescript",
+                    "tsx",
                     "go",
                     "java"
                 },
