8cb26a5a4ba8999501ef8115bc16211c3a9fd669
tenmo <tenmo2003@gmail.com>
2025-06-30 17:59:02 +0700

    add codeium status in status line

--- lua/anhvn/plugins/windsurf.lua
+++ lua/anhvn/plugins/windsurf.lua
@@ -9,8 +9,14 @@ return {
             require("codeium").setup({
                 virtual_text = {
                     enabled = true,
+                    filetypes = {
+                        oil = false
+                    }
                 }
             })
+
+            vim.o.statusline = "%<%f %h%w%m%r %=%{% &showcmdloc == 'statusline' ? '%-10.S ' : '' %}%{% exists('b:keymap_name') ? '<'..b:keymap_name..'> ' : '' %}%{% &ruler ? ( &rulerformat == '' ? '%-14.(%l,%c%V%) %P' : &rulerformat ) : '' %}"
+            vim.o.statusline = vim.o.statusline .. "%3{v:lua.require('codeium.virtual_text').status_string()}"
         end
     },
 }
