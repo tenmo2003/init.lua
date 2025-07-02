6401d45b8fd1c3ef19b4f7b148909dd9ffaeb800
tenmo <tenmo2003@gmail.com>
2025-07-02 14:31:19 +0700

    alias set in remap.lua

--- plugin/remap.lua
+++ plugin/remap.lua
@@ -1,48 +1,50 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+local set = vim.keymap.set
 
-vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
-vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
-vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move current line down" })
-vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move current line up" })
-vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi", { desc = "Move current line down" })
-vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi", { desc = "Move current line up" })
+set("n", "<leader>pv", vim.cmd.Ex)
+
+set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move current line down" })
+set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move current line up" })
+set("i", "<A-j>", "<Esc>:m .+1<CR>==gi", { desc = "Move current line down" })
+set("i", "<A-k>", "<Esc>:m .-2<CR>==gi", { desc = "Move current line up" })
 
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+set("n", "J", "mzJ`z")
+set("n", "<C-d>", "<C-d>zz")
+set("n", "<C-u>", "<C-u>zz")
+set("n", "n", "nzzzv")
+set("n", "N", "Nzzzv")
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+set({ "n", "v" }, "<leader>y", [["+y]])
+set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
+set({ "n", "v" }, "<leader>d", "\"_d")
 
 -- This is going to get me cancelled
-vim.keymap.set("i", "<C-c>", "<Esc>")
+set("i", "<C-c>", "<Esc>")
 
-vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+set("n", "Q", "<nop>")
+set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
-vim.keymap.set("n", "<leader>fc", function()
+set("n", "<leader>fc", function()
     require("conform").format({ bufnr = 0 })
     print("Formatted")
 end, { desc = "Format with conform" })
 
-vim.keymap.set("n", "<leader>fb", "mzgg=G`z", { desc = "Built-in format" })
+set("n", "<leader>fb", "mzgg=G`z", { desc = "Built-in format" })
 
-vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+set("n", "<C-k>", "<cmd>cprev<CR>zz")
+set("n", "<C-j>", "<cmd>cnext<CR>zz")
+set("n", "<leader>k", "<cmd>lnext<CR>zz")
+set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Change all occurrences of word under the cursor" })
-vim.keymap.set("v", "<leader>s", [["zy:%s/\<<C-r>z\>/<C-r>z/gI<Left><Left><Left>]], { desc = "Change all occurrences of selection" })
-vim.keymap.set("n", "<leader>cx", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Set executable"})
+set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Change all occurrences of word under the cursor" })
+set("v", "<leader>s", [["zy:%s/\<<C-r>z\>/<C-r>z/gI<Left><Left><Left>]], { desc = "Change all occurrences of selection" })
+set("n", "<leader>cx", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Set executable"})
 
-vim.keymap.set("n", "<leader>qc", function()
+set("n", "<leader>qc", function()
   vim.cmd("cclose")
   vim.fn.setqflist({})
 end, { desc = "Close and clear quickfix list" })
