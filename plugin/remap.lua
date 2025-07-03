8e80f652282f9b4271492ad6f9dcc8d8336cdc01
tenmo <tenmo2003@gmail.com>
2025-07-03 15:07:17 +0700

    add keymap to close tab

--- plugin/remap.lua
+++ plugin/remap.lua
@@ -9,7 +9,6 @@ set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move current line up" })
 set("i", "<A-j>", "<Esc>:m .+1<CR>==gi", { desc = "Move current line down" })
 set("i", "<A-k>", "<Esc>:m .-2<CR>==gi", { desc = "Move current line up" })
 
-
 set("n", "J", "mzJ`z")
 set("n", "<C-d>", "<C-d>zz")
 set("n", "<C-u>", "<C-u>zz")
@@ -20,7 +19,7 @@ set("n", "N", "Nzzzv")
 set({ "n", "v" }, "<leader>y", [["+y]])
 set("n", "<leader>Y", [["+Y]])
 
-set({ "n", "v" }, "<leader>d", "\"_d")
+set({ "n", "v" }, "<leader>d", '"_d')
 
 -- This is going to get me cancelled
 set("i", "<C-c>", "<Esc>")
@@ -29,8 +28,8 @@ set("n", "Q", "<nop>")
 set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
 set("n", "<leader>fc", function()
-    require("conform").format({ bufnr = 0 })
-    print("Formatted")
+    require("conform").format { bufnr = 0 }
+    print "Formatted"
 end, { desc = "Format with conform" })
 
 set("n", "<leader>fb", "mzgg=G`z", { desc = "Built-in format" })
@@ -40,11 +39,25 @@ set("n", "<C-j>", "<cmd>cnext<CR>zz")
 set("n", "<leader>k", "<cmd>lnext<CR>zz")
 set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Change all occurrences of word under the cursor" })
-set("v", "<leader>s", [["zy:%s/\<<C-r>z\>/<C-r>z/gI<Left><Left><Left>]], { desc = "Change all occurrences of selection" })
-set("n", "<leader>cx", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Set executable"})
+set(
+    "n",
+    "<leader>s",
+    [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+    { desc = "Change all occurrences of word under the cursor" }
+)
+set(
+    "v",
+    "<leader>s",
+    [["zy:%s/\<<C-r>z\>/<C-r>z/gI<Left><Left><Left>]],
+    { desc = "Change all occurrences of selection" }
+)
+set("n", "<leader>cx", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Set executable" })
 
 set("n", "<leader>qc", function()
-  vim.cmd("cclose")
-  vim.fn.setqflist({})
+    vim.cmd "cclose"
+    vim.fn.setqflist {}
 end, { desc = "Close and clear quickfix list" })
+
+set("n", "<leader>tc", function()
+    vim.cmd "tabclose"
+end)
