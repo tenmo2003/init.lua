2f891c7d17278a35b590f585d02f497196d95b97
tenmo <tenmo2003@gmail.com>
2025-07-03 09:41:55 +0700

    add format on save for conform

--- lua/anhvn/plugins/conform.lua
+++ lua/anhvn/plugins/conform.lua
@@ -1,16 +1,17 @@
 return {
-    'stevearc/conform.nvim',
-    opts = {},
-    config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-                lua = { "stylua" },
-                go = { "gofmt" },
-                javascript = { "prettierd" },
-                javascriptreact = { "prettierd" },
-                typescript = { "prettierd" },
-                typescriptreact = { "prettierd" },
-            }
-        })
-    end
+	"stevearc/conform.nvim",
+	opts = {
+        format_on_save = {
+            timeout_ms = 500,
+            lsp_fallback = true,
+        },
+		formatters_by_ft = {
+			lua = { "stylua" },
+			go = { "gofmt" },
+			javascript = { "prettierd" },
+			javascriptreact = { "prettierd" },
+			typescript = { "prettierd" },
+			typescriptreact = { "prettierd" },
+		},
+	},
 }
