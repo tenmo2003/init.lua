c9e761da541d56705bd460733b11050b6fdd342f
tenmo <tenmo2003@gmail.com>
2025-07-04 17:48:10 +0700

    adjust lsp config according to the documentation

--- lua/anhvn/plugins/lsp.lua
+++ lua/anhvn/plugins/lsp.lua
@@ -79,20 +79,21 @@ return {
             ensure_installed = ensure_installed,
         }
 
-        require("mason-lspconfig").setup {
-            automatic_enable = true,
-        }
+        -- NOTE: refer to https://neovim.io/doc/user/lsp.html#lsp-config
+        vim.lsp.config("*", {
+            capabilities = capabilities,
+        })
 
         for name, config in pairs(language_servers) do
             if config == true then
                 config = {}
             end
-            config = vim.tbl_deep_extend("force", {}, {
-                capabilities = capabilities,
-            }, config)
-
-            lspconfig[name].setup(config)
+            vim.lsp.config(name, config)
         end
+
+        require("mason-lspconfig").setup {
+            automatic_enable = true,
+        }
 
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
