b1e1933fd2ab453867bc60b98f0a997d3cce39cd
tenmo <tenmo2003@gmail.com>
2025-07-08 15:26:08 +0700

    adjust line number highlighting colours

--- lua/anhvn/plugins/color.lua
+++ lua/anhvn/plugins/color.lua
@@ -2,10 +2,19 @@ return {
     {
         "catppuccin/nvim",
         name = "catppuccin",
-        priority = 1000 ,
+        priority = 1000,
         config = function()
+            require("catppuccin").setup {
+                custom_highlights = function(colors)
+                    return {
+                        LineNr = { fg = colors.text },
+                        LineNrAbove = { fg = colors.surface2 },
+                        LineNrBelow = { fg = colors.surface2 },
+                    }
+                end,
+            }
             vim.cmd.colorscheme "catppuccin"
-        end
+        end,
     },
     -- {
     --     "vague2k/vague.nvim",
