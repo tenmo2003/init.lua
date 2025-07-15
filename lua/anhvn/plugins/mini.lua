b22c88cdd2480c1cdf03e92c66b2b06b774d061f
tenmo <tenmo2003@gmail.com>
2025-07-15 14:03:44 +0700

    increase mini.ai coverage

--- lua/anhvn/plugins/mini.lua
+++ lua/anhvn/plugins/mini.lua
@@ -1,23 +1,24 @@
 return {
     {
-        'echasnovski/mini.ai',
+        "echasnovski/mini.ai",
         version = false,
         opts = {
             custom_textobjects = {
                 g = function()
                     local from = { line = 1, col = 1 }
                     local to = {
-                        line = vim.fn.line('$'),
-                        col = math.max(vim.fn.getline('$'):len(), 1)
+                        line = vim.fn.line "$",
+                        col = math.max(vim.fn.getline("$"):len(), 1),
                     }
                     return { from = from, to = to }
-                end
-            }
-        }
+                end,
+            },
+            n_lines = 80,
+        },
     },
     {
-        'echasnovski/mini.splitjoin',
+        "echasnovski/mini.splitjoin",
         version = false,
-        opts = {}
+        opts = {},
     },
 }
