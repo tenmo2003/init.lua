ea0de8894f4815e16cfeddc96c6f683e331ca049
tenmo <tenmo2003@gmail.com>
2025-08-25 17:54:29 +0700

    add autocmd to reapply statusline on colorscheme change

--- lua/anhvn/plugins/color.lua
+++ lua/anhvn/plugins/color.lua
@@ -1,3 +1,20 @@
+local function colorscheme_autocmd(colorscheme_pattern, additional_callback)
+    local augroup = vim.api.nvim_create_augroup
+    local autocmd = vim.api.nvim_create_autocmd
+    local anhvn = augroup("colorscheme-" .. colorscheme_pattern, {})
+
+    autocmd("ColorScheme", {
+        group = anhvn,
+        pattern = colorscheme_pattern,
+        callback = function()
+            vim.cmd.source(vim.fn.stdpath "config" .. "/plugin/statusline.lua")
+            if additional_callback and type(additional_callback) == "function" then
+                additional_callback()
+            end
+        end,
+    })
+end
+
 return {
     {
         "catppuccin/nvim",
@@ -13,14 +30,17 @@ return {
                     }
                 end,
             }
+            colorscheme_autocmd "catppuccin*"
             -- vim.cmd.colorscheme "catppuccin-macchiato"
         end,
     },
     {
         "vague2k/vague.nvim",
         config = function()
+            colorscheme_autocmd("vague", function()
+                vim.cmd.hi "StatusLine guibg=NONE"
+            end)
             vim.cmd.colorscheme "vague"
-            vim.cmd ":hi statusline guibg=NONE"
         end,
     },
 }
