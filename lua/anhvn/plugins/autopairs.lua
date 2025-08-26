6613e30475288f66641e95cc8ab59d8663b4fb54
tenmo <tenmo2003@gmail.com>
2025-08-26 11:18:52 +0700

    change autopairs plugin

--- lua/anhvn/plugins/autopairs.lua
+++ lua/anhvn/plugins/autopairs.lua
@@ -1,3 +1,12 @@
 return {
-    "jiangmiao/auto-pairs"
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        -- config = true,
+        opts = {
+            fast_wrap = {},
+        },
+        -- use opts = {} for passing setup options
+        -- this is equivalent to setup({}) function
+    },
 }
