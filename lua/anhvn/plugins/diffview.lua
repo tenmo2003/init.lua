fded944daa0468213f7b21764f962abe6e538f67
tenmo <tenmo2003@gmail.com>
2025-09-05 10:43:10 +0700

    add keymap for current file git history

--- lua/anhvn/plugins/diffview.lua
+++ lua/anhvn/plugins/diffview.lua
@@ -1,3 +1,17 @@
 return {
-    { "sindrets/diffview.nvim" }
+    {
+        "sindrets/diffview.nvim",
+        opts = {
+            use_icons = false,
+        },
+        keys = {
+            {
+                "<leader>gh",
+                function()
+                    vim.cmd "DiffviewFileHistory %"
+                end,
+                desc = "View current file git history",
+            },
+        },
+    },
 }
