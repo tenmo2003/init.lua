9c183f16373ad37bb1c38766408bdf05887d32ec
tenmo <tenmo2003@gmail.com>
2025-09-11 21:57:25 +0700

    add treesitter textobjects

--- lua/anhvn/plugins/treesitter.lua
+++ lua/anhvn/plugins/treesitter.lua
@@ -4,8 +4,24 @@ return {
         branch = "master",
         lazy = false,
         build = ":TSUpdate",
+        dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
         config = function()
             require("nvim-treesitter.configs").setup {
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            -- You can use the capture groups defined in textobjects.scm
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            ["ic"] = "@class.inner",
+                            ["ar"] = "@return.outer",
+                            ["ir"] = "@return.inner",
+                        },
+                    },
+                },
                 -- A list of parser names, or "all" (the listed parsers MUST always be installed)
                 ensure_installed = {
                     "c",
