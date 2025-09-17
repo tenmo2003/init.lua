3ba5cf278ce8dc2380c724662331b469d797d495
tenmo <tenmo2003@gmail.com>
2025-09-17 16:52:40 +0700

    mini.icons: mock nvim-web-devicons

--- lua/anhvn/plugins/oil.lua
+++ lua/anhvn/plugins/oil.lua
@@ -1,7 +1,15 @@
 return {
     "stevearc/oil.nvim",
     -- Optional dependencies
-    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    dependencies = {
+        {
+            "echasnovski/mini.icons",
+            config = function()
+                require("mini.icons").setup()
+                MiniIcons.mock_nvim_web_devicons()
+            end,
+        },
+    },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
