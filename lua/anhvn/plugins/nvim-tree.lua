21fc08443a66b53ec498f8fbd90599723b49940f
tenmo <tenmo2003@gmail.com>
2025-09-17 10:03:35 +0700

    add some nvim-tree mappings

--- lua/anhvn/plugins/nvim-tree.lua
+++ lua/anhvn/plugins/nvim-tree.lua
@@ -6,15 +6,53 @@ return {
             { "<leader>nt", "<cmd>NvimTreeFindFileToggle<cr>", desc = "Toggle NvimTree" },
         },
         config = function()
-            local function opts(desc)
-                return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+            local api = require "nvim-tree.api"
+
+            local function edit_or_open()
+                local node = api.tree.get_node_under_cursor()
+
+                if node.nodes ~= nil then
+                    -- expand or collapse folder
+                    api.node.open.edit()
+                else
+                    -- open file
+                    api.node.open.edit()
+                    -- Close the tree if file was opened
+                    api.tree.close()
+                end
+            end
+
+            -- open as vsplit on current node
+            local function vsplit_preview()
+                local node = api.tree.get_node_under_cursor()
+
+                if node.nodes ~= nil then
+                    -- expand or collapse folder
+                    api.node.open.edit()
+                else
+                    -- open file as vsplit
+                    api.node.open.vertical()
+                end
+
+                -- Finally refocus on tree if it was lost
+                api.tree.focus()
+            end
+
+            local function change_root_to_global_cwd()
+                local global_cwd = vim.fn.getcwd(-1, -1)
+                api.tree.change_root(global_cwd)
             end
 
             local function my_on_attach(bufnr)
-                local api = require "nvim-tree.api"
-
+                local function opts(desc)
+                    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+                end
                 -- default mappings
                 api.config.mappings.default_on_attach(bufnr)
+
+                vim.keymap.set("n", "_", change_root_to_global_cwd, opts "Change root to global cwd")
+                vim.keymap.set("n", "l", edit_or_open, opts "Edit Or Open")
+                vim.keymap.set("n", "L", vsplit_preview, opts "Vsplit Preview")
             end
 
             require("nvim-tree").setup {
