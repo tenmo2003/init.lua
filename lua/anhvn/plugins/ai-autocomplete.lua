085d40958829c431163d315e2045d1831e095d88
tenmo <tenmo2003@gmail.com>
2025-10-16 14:51:36 +0700

    make supermaven load on InsertEnter

--- lua/anhvn/plugins/ai-autocomplete.lua
+++ lua/anhvn/plugins/ai-autocomplete.lua
@@ -26,6 +26,7 @@ return {
     -- },
     {
         "supermaven-inc/supermaven-nvim",
+        event = "InsertEnter",
         config = function()
             require("supermaven-nvim").setup {
                 ignore_filetypes = {
