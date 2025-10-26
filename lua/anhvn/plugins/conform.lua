440ec3da75a377462f307438022e37d2df298ef6
tenmo <tenmo2003@gmail.com>
2025-10-26 20:40:09 +0700

    make conform.nvim lazy loaded

--- lua/anhvn/plugins/conform.lua
+++ lua/anhvn/plugins/conform.lua
@@ -1,48 +1,52 @@
 return {
     "stevearc/conform.nvim",
-    config = function()
-        local conform = require "conform"
-
-        conform.setup {
-            format_on_save = function(bufnr)
-                local ignore_filetypes = { "java" }
-                if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
-                    return nil
+    event = { "BufWritePre" },
+    keys = {
+        {
+            "<leader>fc",
+            function()
+                if require("conform").format() then
+                    vim.notify "Formatted"
                 end
-                return {
-                    timeout_ms = 500,
-                }
             end,
-            formatters = {
-                prettierdjava = {
-                    command = "prettierd",
-                    args = { "$FILENAME" },
-                },
-                prettierjava = {
-                    command = "prettier",
-                    args = { "--stdin-filepath", "$FILENAME" },
-                    range_args = function(self, ctx)
-                        return { "--range-start", ctx.range["start"], "--range-end", ctx.range["end"] }
-                    end,
-                },
+            mode = "",
+            desc = "Format with conform",
+        },
+    },
+    opts = {
+        format_on_save = function(bufnr)
+            local ignore_filetypes = { "java" }
+            if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+                return nil
+            end
+            return {
+                timeout_ms = 500,
+            }
+        end,
+        formatters = {
+            prettierdjava = {
+                command = "prettierd",
+                args = { "$FILENAME" },
             },
-            formatters_by_ft = {
-                lua = { "stylua" },
-                go = { "gofmt", "goimports" },
-                javascript = { "prettierd" },
-                javascriptreact = { "prettierd" },
-                typescript = { "prettierd" },
-                typescriptreact = { "prettierd" },
-                java = { "prettierdjava" },
+            prettierjava = {
+                command = "prettier",
+                args = { "--stdin-filepath", "$FILENAME" },
+                range_args = function(self, ctx)
+                    return { "--range-start", ctx.range["start"], "--range-end", ctx.range["end"] }
+                end,
             },
-            default_format_opts = {
-                lsp_format = "fallback",
-            },
-        }
-
-        vim.keymap.set("", "<leader>fc", function()
-            conform.format { bufnr = 0 }
-            print "Formatted"
-        end, { desc = "Format with conform" })
-    end,
+        },
+        formatters_by_ft = {
+            lua = { "stylua" },
+            go = { "gofmt", "goimports" },
+            javascript = { "prettierd" },
+            javascriptreact = { "prettierd" },
+            typescript = { "prettierd" },
+            typescriptreact = { "prettierd" },
+            java = { "prettierdjava" },
+        },
+        default_format_opts = {
+            lsp_format = "fallback",
+        },
+    },
 }
