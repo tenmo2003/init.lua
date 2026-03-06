54146249cf9c85eddb5f061bfde5768b7a596de7
tenmo <tenmo2003@gmail.com>
2026-03-06 11:46:32 +0700

    feat(java): add command to quickly launch a debug session

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -255,6 +255,114 @@ end, {
 })
 
 vim.keymap.set("n", "<leader>rm", "<Cmd>RunMain<CR>", { desc = "Run Main Java" })
+
+vim.api.nvim_create_user_command("DebugMain", function(args)
+    local buf = vim.api.nvim_get_current_buf()
+    local filetype = vim.bo[buf].filetype
+
+    if filetype ~= "java" then
+        vim.notify("DebugMain: Not a Java file", vim.log.levels.ERROR)
+        return
+    end
+
+    local ok_dap, dap = pcall(require, "dap")
+    if not ok_dap then
+        vim.notify("DebugMain: nvim-dap not found", vim.log.levels.ERROR)
+        return
+    end
+
+    vim.notify("DebugMain: Detecting a running jdtls client", vim.log.levels.INFO)
+    local wait_success = vim.wait(5000, function()
+        local clients = vim.lsp.get_clients { name = "jdtls" }
+        return #clients > 0
+    end, 500)
+
+    if not wait_success then
+        vim.notify("DebugMain: Timed out waiting for jdtls to start", vim.log.levels.ERROR)
+        return
+    end
+
+    vim.notify("DebugMain: Found jdtls client", vim.log.levels.INFO)
+
+    local filepath = vim.fn.expand "%:p"
+    local filename = vim.fn.expand "%:t:r"
+
+    local package_path = filepath:match "src/main/java/(.+)/[^/]+%.java$" or filepath:match "src/java/(.+)/[^/]+%.java$"
+    if not package_path then
+        vim.notify("DebugMain: Could not determine package structure", vim.log.levels.ERROR)
+        return
+    end
+
+    local fully_qualified_class = package_path:gsub("/", ".") .. "." .. filename
+
+    local project_root = filepath:match "(.+)/src/"
+    if not project_root then
+        vim.notify("DebugMain: Could not find project root", vim.log.levels.ERROR)
+        return
+    end
+
+    local prog_args = {}
+    if args.args ~= "" then
+        for arg in args.args:gmatch "%S+" do
+            table.insert(prog_args, arg)
+        end
+    end
+
+    local has_maven = vim.fn.filereadable(project_root .. "/pom.xml") == 1
+    local has_gradle = vim.fn.filereadable(project_root .. "/build.gradle") == 1
+        or vim.fn.filereadable(project_root .. "/build.gradle.kts") == 1
+
+    local jdwp_port = 8000
+
+    local run_cmd
+    if has_maven then
+        run_cmd = string.format(
+            "cd %s && mvnDebug exec:java -Dexec.mainClass=%s",
+            vim.fn.shellescape(project_root),
+            vim.fn.shellescape(fully_qualified_class)
+        )
+    elseif has_gradle then
+        run_cmd = string.format("cd %s && ./gradlew bootRun --debug-jvm", vim.fn.shellescape(project_root))
+    else
+        vim.notify("DebugMain: No Maven or Gradle build file found", vim.log.levels.ERROR)
+        return
+    end
+
+    if #prog_args > 0 then
+        if has_maven then
+            run_cmd = run_cmd .. " -Dspring-boot.run.arguments=" .. table.concat(prog_args, ",")
+        elseif has_gradle then
+            run_cmd = run_cmd .. " --args='" .. table.concat(prog_args, " ") .. "'"
+        end
+    end
+
+    -- TODO: fallback to new tab
+    local tmux_cmd =
+        string.format("tmux neww -n 'Spring Debug' 'bash -c \"%s\"; echo; echo Press enter to close...; read'", run_cmd)
+    vim.cmd("silent !" .. tmux_cmd)
+
+    local try_attach = function()
+        vim.notify("DebugMain: Attaching DAP to localhost:" .. jdwp_port, vim.log.levels.INFO)
+
+        dap.run {
+            type = "java",
+            request = "attach",
+            name = "Attach Spring: " .. fully_qualified_class,
+            hostName = "localhost",
+            port = jdwp_port,
+            projectName = vim.fn.fnamemodify(project_root, ":t"),
+        }
+    end
+
+    try_attach()
+    vim.notify("DebugMain: Attached DAP to port:" .. jdwp_port, vim.log.levels.INFO)
+end, {
+    desc = "Debug Spring Boot Main via JDWP attach",
+    nargs = "*",
+})
+
+vim.keymap.set("n", "<leader>dm", "<Cmd>DebugMain<CR>", { desc = "Debug Main Java" })
+
 vim.keymap.set("v", "<leader>tc", function()
     local esc = vim.api.nvim_replace_termcodes("<esc>", true, false, true)
     vim.api.nvim_feedkeys(esc, "x", false) -- 'x' flag forces immediate execution
