6688a88c774e29b4d344dd36fa2a3d3481bc19c5
tenmo <tenmo2003@gmail.com>
2026-06-18 14:07:20 +0700

    feat(treesitter): replace tree-sitter-manager with nvim-treesitter again

--- lua/anhvn/plugins/treesitter.lua
+++ lua/anhvn/plugins/treesitter.lua
@@ -1,10 +1,112 @@
 ---@diagnostic disable: missing-fields
 return {
     {
-        "romus204/tree-sitter-manager.nvim",
-        dependencies = {}, -- tree-sitter CLI must be installed system-wide
+        "nvim-treesitter/nvim-treesitter",
+        lazy = false,
+        build = ":TSUpdate",
         config = function()
-            require("tree-sitter-manager").setup()
+            local ts = require "nvim-treesitter"
+
+            local parser_ft = {
+                angular = { "htmlangular" },
+                bash = { "sh" },
+                bibtex = { "bib" },
+                c_sharp = { "cs", "csharp" },
+                commonlisp = { "lisp" },
+                cooklang = { "cook" },
+                devicetree = { "dts" },
+                diff = { "gitdiff" },
+                eex = { "eelixir" },
+                elixir = { "ex" },
+                embedded_template = { "eruby" },
+                erlang = { "erl" },
+                facility = { "fsd" },
+                faust = { "dsp" },
+                gdshader = { "gdshaderinc" },
+                git_config = { "gitconfig" },
+                git_rebase = { "gitrebase" },
+                glimmer = { "handlebars", "html.handlebars" },
+                godot_resource = { "gdresource" },
+                haskell = { "hs" },
+                haskell_persistent = { "haskellpersistent" },
+                idris = { "idris2" },
+                ini = { "confini", "dosini" },
+                janet_simple = { "janet" },
+                javascript = { "javascriptreact", "ecma", "ecmascript", "jsx", "js" },
+                json = { "jsonc" },
+                glimmer_javascript = { "javascript.glimmer" },
+                latex = { "tex" },
+                linkerscript = { "ld" },
+                m68k = { "asm68k" },
+                make = { "automake" },
+                markdown = { "pandoc" },
+                muttrc = { "neomuttrc" },
+                ocaml_interface = { "ocamlinterface" },
+                perl = { "pl" },
+                poe_filter = { "poefilter" },
+                powershell = { "ps1" },
+                properties = { "jproperties" },
+                python = { "py", "gyp" },
+                qmljs = { "qml" },
+                runescript = { "clientscript" },
+                scala = { "sbt" },
+                slang = { "shaderslang" },
+                ssh_config = { "sshconfig" },
+                starlark = { "bzl" },
+                surface = { "sface" },
+                systemverilog = { "verilog" },
+                t32 = { "trace32" },
+                tcl = { "expect" },
+                terraform = { "terraform-vars" },
+                textproto = { "pbtxt" },
+                tlaplus = { "tla" },
+                tsx = { "typescriptreact", "typescript.tsx" },
+                typescript = { "ts" },
+                glimmer_typescript = { "typescript.glimmer" },
+                typst = { "typ" },
+                udev = { "udevrules" },
+                uxntal = { "tal", "uxn" },
+                v = { "vlang" },
+                vhs = { "tape" },
+                xml = { "xsd", "xslt", "svg" },
+                xresources = { "xdefaults" },
+            }
+
+            for lang, fts in pairs(parser_ft) do
+                for _, ft in ipairs(fts) do
+                    vim.treesitter.language.register(lang, ft)
+                end
+            end
+
+            ts.setup {
+                -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
+                install_dir = vim.fn.stdpath "data" .. "/site",
+            }
+
+            ts.install {
+                "c",
+                "cpp",
+                "lua",
+                "vim",
+                "vimdoc",
+                "query",
+                "markdown",
+                "markdown_inline",
+                "yaml",
+                "javascript",
+                "typescript",
+                "tsx",
+                "go",
+                "java",
+                "rust",
+            }
+
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = { "*" },
+                callback = function()
+                    pcall(vim.treesitter.start)
+                end,
+            })
         end,
     },
     { "nvim-treesitter/nvim-treesitter-context", opts = {} },
