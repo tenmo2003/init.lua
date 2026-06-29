371c49440ef639faa54f1d726c2a46df8c2dd428
tenmo <tenmo2003@gmail.com>
2026-06-29 11:33:47 +0700

    feat: recover LspInfo and LspLog command

--- lua/anhvn/init.lua
+++ lua/anhvn/init.lua
@@ -170,3 +170,12 @@ vim.fn.mkdir(shada_path, "p")
 
 local cwd = vim.fn.getcwd():gsub("/", "%%"):gsub(":", "%%")
 vim.opt.shadafile = shada_path .. cwd .. ".shada"
+
+vim.api.nvim_create_user_command("LspInfo", function()
+    vim.cmd "checkhealth vim.lsp"
+end, {})
+
+vim.api.nvim_create_user_command("LspLog", function()
+    vim.cmd "tabnew"
+    vim.cmd("edit " .. vim.lsp.log.get_filename())
+end, {})
